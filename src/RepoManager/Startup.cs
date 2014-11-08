d5a8f93b6f3f4ba4eea7ec1080a4f87bd85a0a41
Glenn Condron <glennc@microsoft.com>
2014-11-07 16:48:47 -0800

    Initial structure

---
+++ src/RepoManager/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Mvc;
+
+namespace RepoManager
+{
+    public class Startup
+    {
+        IConfiguration Configuration;
+
+        public Startup()
+        {
+            Configuration = new Configuration()
+                .AddIniFile("configuration.ini")
+                .AddEnvironmentVariables();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+    }
+}
