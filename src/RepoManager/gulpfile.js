386be468d06412c6d64726555bbd35bb8bd0fc2f
Glenn Condron <glennc@microsoft.com>
2014-11-09 11:26:39 -0800

    Slightly altered folder structure. Change to gulp.

---
+++ src/RepoManager/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+var bower = require('gulp-bower');
+
+gulp.task('bower', function () {
+    return bower()
+      .pipe(gulp.dest('wwwroot/lib/'))
+});
+
+gulp.task('default', ['bower'], function () {
+    //tcopy font-awesome without a plugin
+    gulp.src("./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}")
+        .pipe(gulp.dest("./wwwroot/fonts"));
+});
