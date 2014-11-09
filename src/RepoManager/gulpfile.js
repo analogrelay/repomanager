e8b4b254b393077765bd41a1de735f1ab487b333
Glenn Condron <glennc@microsoft.com>
2014-11-09 11:45:06 -0800

    Add SASS

--- src/RepoManager/gulpfile.js
+++ src/RepoManager/gulpfile.js
@@ -1,12 +1,19 @@
 ﻿var gulp = require('gulp');
 var bower = require('gulp-bower');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./client/styles/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./wwwroot/css'));
+});
 
 gulp.task('bower', function () {
     return bower()
       .pipe(gulp.dest('wwwroot/lib/'))
 });
 
-gulp.task('default', ['bower'], function () {
+gulp.task('default', ['bower', 'sass'], function () {
     //tcopy font-awesome without a plugin
     gulp.src("./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}")
         .pipe(gulp.dest("./wwwroot/fonts"));
